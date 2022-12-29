295fb84cf1cb2fe2ebfefff16d8e3ef33c2b87f0
Guilherme Gonçalves Pataro <88072413+PataroGuilherme@users.noreply.github.com>
2022-12-29 12:15:47 -0300

    Updated || 29-12-2022
    Updated || 29-12-2022

---
+++ Teams_GpuDisable.ps1
@@ -0,0 +1,21 @@
+﻿
+#01. Stopping Teams service, if necessary.
+
+$Process = Get-Process | Where name -EQ "Teams"
+
+ForEach ($App in $Process) {
+
+    Stop-Process $App -Force
+
+}
+
+
+#02. Editing the variable "disableGpu"
+
+[String]$FilePath = ($env:USERPROFILE + "\AppData\Roaming\Microsoft\Teams\desktop-config.json")
+[String]$FileContent = (Get-Content $FilePath)
+
+
+$Modification = $FileContent -replace ("`"disableGpu`":false", "`"disableGpu`":true")  
+
+$Modification > $FilePath
