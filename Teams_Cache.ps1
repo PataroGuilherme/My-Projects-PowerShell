d024d62b87bd4429dcb3fa7d965975edc62caaf4
Guilherme Gonçalves Pataro <88072413+PataroGuilherme@users.noreply.github.com>
2023-01-04 01:16:41 -0300

    Updated || 04-01-2023
    Updated || 04-01-2023

---
+++ Teams_Cache.ps1
@@ -0,0 +1,51 @@
+﻿
+#01. Stopping Teams service, if necessary.
+
+$Process = Get-Process | Where name -EQ "Teams"
+
+ForEach ($App in $Process) {
+
+    Stop-Process $App -Force
+
+}
+
+
+#02. Editing the variable "disableGpu"
+
+[String]$FilePath = ($env:USERPROFILE + "\AppData\Roaming\Microsoft\Teams\desktop-config.json")
+[String]$FileContent = (Get-Content $FilePath)
+
+
+$Modification = $FileContent -replace ("`"disableGpu`":false", "`"disableGpu`":true")  
+
+$Modification > $FilePath
+
+
+#03. Remove Cache files from MS Teams.
+
+
+[String]$CachePath = "\AppData\Roaming\Microsoft\Teams\Cache"
+
+[String]$CompletePath = ($env:USERPROFILE + $CachePath)
+
+Set-Location ($CompletePath)
+
+Get-ChildItem | ForEach-Object {
+
+   Remove-Item $PSItem -Force
+
+}
+
+ipconfig /flushdns
+
+
+# 04. Removing Variables.
+
+<#
+Remove-Variable $CompletePath
+Remove-Variable $CachePath
+Remove-Variable $Modification
+Remove-Variable $FileContent
+Remove-Variable $FilePath
+Remove-Variable $Process
+#>
